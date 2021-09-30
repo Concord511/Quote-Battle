5853d8b71e2d9c153ba78fa7fac3c789bc076721
mmelendez3 <mele0019@gmail.com>
2021-09-30 13:58:59 -0500

    initial commit with quote garden API fetch setup

---
+++ assets/js/scripts.js
@@ -0,0 +1,47 @@
+
+
+
+function randomQuote() {
+    //make a fetch request to quote garden
+    fetch(`https://quote-garden.herokuapp.com/api/v3/quotes/random`)
+      
+    
+    .then(function(randomQuote){
+      return randomQuote.json()
+    })
+        .then(function(randomQuote){
+          console.log(randomQuote)
+        })
+      }
+
+randomQuote()
+
+  
+  
+  
+  
+
+
+
+// function myFunction() {
+  //   fetch(
+  //     // Make a fetch request to Wikipedia to get a random article title
+  //     `https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=1&origin=*`
+  //     //
+  //   )
+  //     .then(function(wikiResponse) {
+  //       return wikiResponse.json();
+  //     })
+  //     .then(function(wikiResponse) {
+  //       console.log(wikiResponse)
+  //       // Create a variable to hold the title of the Wikipedia article
+  //       // YOUR CODE HERE
+  //       var title = wikiResponse.query.random[0].title
+  //       // Display the article title above the GIF as a <h2> heading
+  //       // YOUR CODE HERE
+  //       var headingEl = document.createElement("h2")
+  //       headingEl.textContent = title
+  //       var responseEl = document.getElementById("response-container")
+  //       responseEl.innerHTML = ""
+  //       responseEl.appendChild(headingEl)
+  //       var rating = document.getElementById("rating").nodeValue
